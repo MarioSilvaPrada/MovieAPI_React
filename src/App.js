2f39eaf4750b07b6d963ed58e6bd82d601f4c172
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-08-06 23:43:32 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import styled from "styled-components";
 
-function App() {
+import { connect } from "react-redux";
+import { getGenres, getMovies } from "./actions/index";
+
+const StyledContainer = styled.div`
+  display: flex;
+`;
+
+const StyledSideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledMovies = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const MovieCard = styled.div`
+  width: 200px;
+  margin: 2em;
+
+  img {
+    width: 200px;
+    border-radius: 5px;
+  }
+  
+`;
+
+function App({ getGenres, genres, getMovies, movies }) {
+  useEffect(() => {
+    getGenres();
+    getMovies("popular");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie API</h1>
+      <StyledContainer>
+        <StyledSideBar>
+          <div>
+            <h3>Discover</h3>
+            <p onClick={() => getMovies("popular")}>Popular</p>
+            <p onClick={() => getMovies("top_rated")}>Top Rated</p>
+            <p onClick={() => getMovies("now_playing")}>Upcoming</p>
+          </div>
+          <h3>Genres</h3>
+          {genres.map((genre, i) => (
+            <p key={i}>{genre.name}</p>
+          ))}
+        </StyledSideBar>
+
+        <div>
+          <StyledMovies>
+            {movies.map((movie, i) => (
+              <MovieCard key={i}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                />
+                <p>{movie.title}</p>
+              </MovieCard>
+            ))}
+          </StyledMovies>
+        </div>
+      </StyledContainer>
     </div>
   );
 }
+const mapStateToProps = state => ({
+  genres: state.get.genres,
+  movies: state.get.movies
+});
 
-export default App;
+export default connect(
+  mapStateToProps,
+  { getGenres, getMovies }
+)(App);
--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,11 +1,23 @@
 import * as TYPES from "./types";
-import tmdb from "../api/tmdb";
+// import tmdb from "../api/tmdb";
+import axios from "axios";
+
+let API_URL = "https://api.themoviedb.org/3/";
+let API_KEY = "api_key=aeb5dba1f1913a3c858099a94105e7c3";
 
 // Get genres from API
 export const getGenres = () => async dispatch => {
-  const res = await tmdb.get("/genre/movie/list");
+  const res = await axios.get(`${API_URL}genre/movie/list?${API_KEY}`);
   dispatch({
     type: TYPES.GET_GENRES,
-    payload: res.data
+    payload: res.data.genres
+  });
+};
+
+export const getMovies = (type) => async dispatch => {
+  const res = await axios.get(`${API_URL}movie/${type}?${API_KEY}&page=1`);
+  dispatch({
+    type: TYPES.GET_DISCOVER,
+    payload: res.data.results
   });
 };
