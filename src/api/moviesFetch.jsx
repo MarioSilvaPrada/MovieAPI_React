5124c8de808bcb9a40e258a606316a3f532cea6d
Mário Prada <mariosilvaprada@gmail.com>
2019-09-18 10:52:16 +0100

    updating

--- src/api/moviesFetch.jsx
+++ src/api/moviesFetch.jsx
@@ -1,2 +1,5 @@
 export const API_URL = "https://api.themoviedb.org/3/";
-export const API_KEY = process.env.REACT_APP_APIKEY;
+// export const API_KEY = process.env.REACT_APP_APIKEY;
+export const API_KEY = 'aeb5dba1f1913a3c858099a94105e7c3';
+
+
