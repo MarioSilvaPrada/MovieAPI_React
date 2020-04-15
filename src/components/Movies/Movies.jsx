4aafcc218d07b9e1f8df4efa28aa7eb9863dc4ba
Mário Prada <mariosilvaprada@gmail.com>
2020-04-15 22:34:27 +0100

    fixing

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { unstable_Profiler as Profiler } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { white } from '../../config/style';
@@ -8,35 +8,26 @@ import { TiStar } from 'react-icons/ti';
 import * as S from './Movies.styled';
 
 const Movies = ({ movies }) => {
-  const callback = (id, phase, actualTime, baseTime, startTime, commitTime) => {
-    console.log(`%c${id}'s ${phase} phase:`, 'background: black; color: #94fc13; padding: .3rem 1rem');
-    console.log(`Actual time: ${actualTime}`);
-    console.log(`Base time: ${baseTime}`);
-    console.log(`Start time: ${startTime}`);
-    console.log(`Commit time: ${commitTime}`);
-}
   return (
     <S.StyledMovies>
       {movies['results']
         // .filter(movie => movie.genre_ids[0] === selectedGenre)
         .map((movie, i) => (
-          <Profiler id={movie.title} onRender={callback}>
-            <S.MovieCard key={i}>
-              <Link to={`/${movie.id}`} style={{ textDecoration: 'none', color: white }}>
-                {movie.poster_path ? (
-                  <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
-                ) : (
-                  <img src={'https://image.flaticon.com/icons/svg/863/863170.svg'} />
-                )}
-              </Link>
+          <S.MovieCard key={i}>
+            <Link to={`/${movie.id}`} style={{ textDecoration: 'none', color: white }}>
+              {movie.poster_path ? (
+                <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
+              ) : (
+                <img src={'https://image.flaticon.com/icons/svg/863/863170.svg'} />
+              )}
+            </Link>
 
-              <p>{movie.title}</p>
-              <S.StyleRating>
-                <TiStar />
-                {movie.vote_average.toFixed(1)}
-              </S.StyleRating>
-            </S.MovieCard>
-          </Profiler>
+            <p>{movie.title}</p>
+            <S.StyleRating>
+              <TiStar />
+              {movie.vote_average.toFixed(1)}
+            </S.StyleRating>
+          </S.MovieCard>
         ))}
     </S.StyledMovies>
   );
